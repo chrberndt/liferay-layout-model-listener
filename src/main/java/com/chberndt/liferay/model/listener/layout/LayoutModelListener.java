cf7ce8f09dd288482bd84b70070fdac76a275108
Christian Berndt <christian.berndt@liferay.com>
2022-02-26 00:05:21 +0100

    Reverse default value for layoutUpdateable.

--- src/main/java/com/chberndt/liferay/model/listener/layout/LayoutModelListener.java
+++ src/main/java/com/chberndt/liferay/model/listener/layout/LayoutModelListener.java
@@ -1,17 +1,49 @@
 package com.chberndt.liferay.model.listener.layout;
 
+import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.sites.kernel.util.Sites;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Christian Berndt
  */
 @Component(service = ModelListener.class)
 public class LayoutModelListener extends BaseModelListener<Layout> {
-	
-	
+
+	@Override
+	public void onBeforeCreate(Layout layout) throws ModelListenerException {
+		try {
+			Group group = _groupGroupLocalService.getGroup(layout.getGroupId());
+
+			if (group.getFriendlyURL(
+				).startsWith(
+					"/template-"
+				)) {
+
+				// Reverse layoutUpdateable default value only
+				// in the realm of site templates
+
+				UnicodeProperties typeSettingsUnicodeProperties =
+					layout.getTypeSettingsProperties();
+
+				typeSettingsUnicodeProperties.setProperty(
+					Sites.LAYOUT_UPDATEABLE, Boolean.FALSE.toString());
+			}
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
+	@Reference
+	private GroupLocalService _groupGroupLocalService;
 
 }
